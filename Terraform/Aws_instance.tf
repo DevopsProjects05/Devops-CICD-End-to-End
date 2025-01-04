6adf423d60bc1cacafb170ae7e5a2ddd2d61bc86
Nuthan Gatla <NuthanGatla@gmail>
2025-01-04 11:52:20 +0530

    terraform

--- Terraform/Aws_instance.tf
+++ Terraform/Aws_instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0fd05997b4dff7aac"                               # Amazon Linux 2 AMI
   instance_type = "t2.micro"
 
-  key_name = "DevOps-Practice"                                       # Replace with your key pair name
+  key_name = "DevOps_Practice"                                       # Replace with your key pair name
 
   tags = {
     Name = "Terraform-Managed-Instance"
