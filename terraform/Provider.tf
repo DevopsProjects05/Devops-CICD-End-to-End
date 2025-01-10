ca7d11fab32326baef56d13f9305f7ecc766a639
Nuthan Gatla <NuthanGatla@gmail>
2025-01-10 08:03:08 +0530

    cicd

--- terraform/Provider.tf
+++ terraform/Provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  access_key = "YOUR_ACCESS_KEY"     # Replace with your access key
-  secret_key = "YOUR_SECRET_KEY"     # Replace with your secret key
+  
   region = "ap-south-1"              # Replace with your region 
 
 
